012dcd99ecae83ab8a37316339bb56ee7ac53b21
erivaniacasanova-jpg <erivaniacasanova@gmail.com>
2025-10-19 19:02:22 -0300

    Update page.tsx

--- app/[representanteId]/page.tsx
+++ app/[representanteId]/page.tsx
@@ -8,39 +8,8 @@ export default async function RepresentantePage({ params }) {
     "134684": {
       id: "134684",
       nome: "William Dos Santos Pessoa",
+      whatsapp: "5521969400194",
     },
-     if (fatherId === "135302") {
-        try {
-          await fetch("https://webhook.fiqon.app/webhook/a027566a-c651-460e-8805-6f6414de55b1/a98d0f5c-c379-4104-abf9-d07124ccd1ff", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              nome: formData.name || "",
-              whatsapp: formData.cell || "",
-              tipoChip: formData.typeChip || "",
-              formaEnvio: formData.typeFrete || "",
-              planoEscolhido: planoEscolhido || "",
-            }),
-          })
-          console.log("Webhook enviado:", {
-            nome: formData.name,
-            whatsapp: formData.cell,
-            tipoChip: formData.typeChip,
-            formaEnvio: formData.typeFrete,
-            planoEscolhido: planoEscolhido,
-          })
-          
-        } catch (webhookError) {
-          console.error("[v0] Webhook error:", webhookError)
-        }
-      }
-    } catch (error) {
-      console.error("[v0] Error submitting form:", error)
-      setSuccessModal(true)
-    }
-  }
     "135302": {
     id: "135302",
     nome: "Antonia Erivania Delmiro Jacinto",
